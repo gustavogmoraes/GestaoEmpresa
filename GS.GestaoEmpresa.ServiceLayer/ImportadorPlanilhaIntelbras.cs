9a0cd91538ab5ee09eaff84bcc043ea27b2e3e27
Gustavo Moraes <gustavog.moraes@outlook.com>
2020-01-21 17:57:43 -0300

    .

---
+++ GS.GestaoEmpresa.ServiceLayer/ImportadorPlanilhaIntelbras.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GS.GestaoEmpresa.ServiceLayer
+{
+    public class ImportadorPlanilhaIntelbras
+    {
+        //https://forums.asp.net/t/2125945.aspx?Read+the+content+of+Excel+Binary+File+XLSB+file+using+EPPlus+library+or+any+other+dll
+    }
+}
